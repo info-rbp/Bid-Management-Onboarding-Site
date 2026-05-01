31a75ad9f57d733339951a2ef4ef9c69fccad1df
Gianpaulo Coletti <info@remotebusinesspartner.com.au>
2026-05-01 10:20:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5630603817-ce290",
+  "appId": "1:371843227042:web:673b8d6c135c9b1230e6fb",
+  "apiKey": "AIzaSyBKd_7JeafE5ZndgDM0R3_ttXjnuk8pu08",
+  "authDomain": "studio-5630603817-ce290.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "371843227042"
+};
