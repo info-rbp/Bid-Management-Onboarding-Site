1fb9a010f67b56723e5a93c471ef36b140146727
Gianpaulo Umberto COLETTI <info@remotebusinesspartner.com.au>
2026-05-08 03:11:22 -0700

    Add Firebase Admin server helper

---
+++ src/lib/firebase-admin.ts
@@ -0,0 +1,21 @@
+import { applicationDefault, getApps, initializeApp } from 'firebase-admin/app';
+import { getAuth } from 'firebase-admin/auth';
+import { getFirestore } from 'firebase-admin/firestore';
+
+function getAdminApp() {
+  if (getApps().length > 0) {
+    return getApps()[0];
+  }
+
+  return initializeApp({
+    credential: applicationDefault(),
+  });
+}
+
+export function getAdminAuth() {
+  return getAuth(getAdminApp());
+}
+
+export function getAdminDb() {
+  return getFirestore(getAdminApp());
+}
